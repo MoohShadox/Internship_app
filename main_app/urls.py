1fe1b4d502baf0966ab01291eb13423e5bd40be4
chekirou <chekirouhakim@gmail.com>
2020-09-03 01:11:22 +0200

    add home

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import base,suggestions_view, load_in_db,clear , select_arret
+from .views import base,suggestions_view, load_in_db,clear , select_arret, load_patterns, get_choice, home
 urlpatterns = [
-    path("",base,name="base"),
     path("load_from_cache/<slug>",load_in_db,name="load-from-cache"),
     path("suggestions/<slug>", suggestions_view.as_view(), name="suggestions"),
     path("select/<annee>/<slug>", select_arret, name="select"),
 
-    path("clear_db",clear,name="clear")
+    path("clear_db",clear,name="clear"),
+    path("load_patterns", load_patterns, name = "load_patterns"),
+    path("get_choice", get_choice, name = "get_choice"),
+    path("home", home, name="home")
 ]
