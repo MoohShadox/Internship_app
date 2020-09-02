4c60585e3cacf0aefd48109081c281acc1f8bcd2
Ouaguenouni Mohamed <ouaguenouni.hachemi@gmail.com>
2020-09-03 01:15:02 +0200

    added download links

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import base,suggestions_view, load_in_db,clear , select_arret
+from .views import base,suggestions_view, load_in_db,clear , select_arret,download_file
 urlpatterns = [
     path("",base,name="base"),
     path("load_from_cache/<slug>",load_in_db,name="load-from-cache"),
     path("suggestions/<slug>", suggestions_view.as_view(), name="suggestions"),
     path("select/<slug>", select_arret, name="select"),
-
+    path("dl_csv",download_file,name="dl_csv"),
     path("clear_db",clear,name="clear")
 ]
